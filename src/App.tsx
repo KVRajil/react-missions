28514a0f057f92865f94e1b6f773728aa7c42547
RajilKV <rajil.kv@constantcontact.com>
2023-08-25 14:14:35 +0530

    Mission3 add mocked data list to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-    const [count, setCount] = useState(0);
+    const mockData = [
+        {
+            id: 1,
+            label: 'Some label 1'
+        },
+        {
+            id: 2,
+            label: 'Some label 2'
+        },
+        {
+            id: 3,
+            label: 'Some label 3'
+        },
+        {
+            id: 4,
+            label: 'Some label 4'
+        },
+        {
+            id: 5,
+            label: 'Some label 5'
+        }
+    ];
+
+    const [message, setMessage] = useState("0 Times clicked")
+    const [count, setCount] = useState(0)
+    const [items, setItems] = useState(mockData);
 
     useEffect(() => {
-        const message = `${count} Times clicked`;
-        document.title = message;
+        document.title = `${count} Times clicked`; // Update document title
     }, [count]);
 
     const onClick = () => {
-        setCount(count + 1);
-    };
+        setCount(count + 1)
+    }
 
     return (
         <div className="container">
             <button onClick={onClick}>Click here</button>
-            <div style={{ marginLeft: "1rem" }}>{count} Times clicked</div>
+            <div style={{ marginLeft: '1rem' }}>{message}</div>
+
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>{item.label}</li>
+                ))}
+            </ul>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
